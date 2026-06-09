8299ea066037ec1f0d861825c137994b8fca095e
Matias Mazzanti <mazzantimatiass@gmail.com>
2026-06-09 17:35:43 -0300

    Hover

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include "common.h"
 #include "protocol.h"
 #include "server.h"
+#include <sys/socket.h>
 
 
 int main(int argc, char* argv[]){
-
-
+    int fd_s = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd_s==-1){
+        perror("socket");
+        return -1;
+    }
+    int result = bind(fd_s, , );
+    printf("Hello world\n");
     return 0;
 }
