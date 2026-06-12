4c1d31d5caa6fed139c30aaa67e766f7fc2af82a
Matias Mazzanti <mazzantimatiass@gmail.com>
2026-06-12 12:37:49 -0300

    ds lib

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include "common.h"
 #include "protocol.h"
 #include "server.h"
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#define DS_SS_IMPLEMENTATION
+#define DS_IO_IMPLEMENTATION
+#include "ds.h"
 
+#define MAX_LEN 1024
 int main(int argc, char* argv[]){
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
     int cfd;
@@ -18,7 +23,6 @@ int main(int argc, char* argv[]){
     struct sockaddr_in addr, client_addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(8000);
-   // addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
     socklen_t addrlen = sizeof(addr);
     int result = bind(sfd, (struct sockaddr *) &addr,  addrlen);
@@ -38,32 +42,56 @@ int main(int argc, char* argv[]){
             perror("accept");
             return -1;
         }
-        while (1) {
-            char buffer[1024] = {0};
+        char buffer[MAX_LEN] = {0};
 
-            ssize_t n = read(cfd, buffer, sizeof(buffer) - 1);
-
-            if (n <= 0)
-                break;
-
-            buffer[n] = '\0';
-
-            printf("Client: %s", buffer);
-
-            printf("Respuesta: ");
-
-            fgets(buffer, sizeof(buffer), stdin);
-
-            write(cfd, "Server: ", 8);
-            write(cfd, buffer, strlen(buffer));
+        int result = read(cfd, buffer, MAX_LEN);
+        if (result == -1){
+            perror("read");
+            continue;
         }
 
+        unsigned int buffer_len = result;
+        printf("Client (%u): %s", buffer_len, buffer);
+
+
+        ds_string_slice request, token;
+        ds_string_slice_init(&request, buffer, buffer_len);
+        ds_string_slice_tokenize(&request, ' ', &token);
+        char *verb = NULL;
+        ds_string_slice_to_owned(&token, &verb);
+        if(strcmp(verb, "GET")!= 0){
+            perror("not get");
+            // TODO respond with 400
+            continue;
+        }
+        ds_string_slice_tokenize(&request, ' ', &token);
+        char *path = NULL;
+        ds_string_slice_to_owned(&token, &path);
+        printf("Path requested: %s\n", path);
+        // el protocol venia con un \r antes del \n
+        ds_string_slice_tokenize(&request, '\n', &token);
+        if (token.len > 0 &&
+            token.str[token.len - 1] == '\r') {
+            token.len--;
+        }
+        char *protocol = NULL;
+        ds_string_slice_to_owned(&token, &protocol);
+        printf("Protocol: %s\n", protocol);
+
+
+        char *content = NULL;
+        int content_len = ds_io_read_file(path+1, &content);
+        int response_len = snprintf(NULL, 0, "%s 200 OK\nContent-Type: text/html\nContent-Length: %d\n\n%s", protocol,  content_len, content);
+        char *response = calloc(response_len+1, sizeof(char));
+        snprintf(response, response_len + 1, "%s 200 OK\nContent-Type: text/html\nContent-Length: %d\n\n%s", protocol, content_len, content);
+
+        write(cfd, response, response_len);
+    }
         result = close(sfd);
         if (result==-1){
             perror("close");
             return -1;
         }
 
-    }
     return 0;
 }
